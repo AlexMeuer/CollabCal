0c8308a1233c79eb825b30235e01e6b24a0dc7b6
Beibhinn Keenan <beibhinn.keenan@converge.io>
2022-11-20 19:29:23 +0000

    Remove unused import

--- apps/collabcal/src/components/calendar/tooltipContent.tsx
+++ apps/collabcal/src/components/calendar/tooltipContent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { AppointmentModel } from "@devexpress/dx-react-scheduler";
 import { AppointmentTooltip } from "@devexpress/dx-react-scheduler-material-ui";
 
 export const TooltipContent: React.FC<AppointmentTooltip.ContentProps> = ({
@@ -11,5 +10,5 @@ export const TooltipContent: React.FC<AppointmentTooltip.ContentProps> = ({
   <AppointmentTooltip.Content {...restProps} appointmentData={appointmentData}>
     <ReactMarkdown>{appointmentData?.description}</ReactMarkdown>
     {children}
-  </AppointmentTooltip.Content>
+  </AppointmentTooltip.Content>1
 );
