f190f816f3f5209448c3ca962baad84f359284bc
Beibhinn Keenan <beibhinn.keenan@converge.io>
2022-11-20 19:28:42 +0000

    Remove custom props

--- apps/collabcal/src/components/calendar/tooltipContent.tsx
+++ apps/collabcal/src/components/calendar/tooltipContent.tsx
@@ -3,17 +3,13 @@ import ReactMarkdown from "react-markdown";
 import { AppointmentModel } from "@devexpress/dx-react-scheduler";
 import { AppointmentTooltip } from "@devexpress/dx-react-scheduler-material-ui";
 
-interface ContentProps extends AppointmentTooltip.ContentProps {
-  appointmentData: AppointmentModel;
-}
-
-export const TooltipContent: React.FC<ContentProps> = ({
+export const TooltipContent: React.FC<AppointmentTooltip.ContentProps> = ({
   children,
   appointmentData,
   ...restProps
 }) => (
   <AppointmentTooltip.Content {...restProps} appointmentData={appointmentData}>
-    <ReactMarkdown>{appointmentData.description}</ReactMarkdown>
+    <ReactMarkdown>{appointmentData?.description}</ReactMarkdown>
     {children}
   </AppointmentTooltip.Content>
 );
