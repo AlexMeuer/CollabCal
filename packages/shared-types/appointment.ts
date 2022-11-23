82ca607eb11f6ebf4636c799a0fd4da583925416
Alex Meuer <github@alexmeuer.com>
2022-11-23 21:20:12 +0000

    Apply theming to tooltip links (#33)
    Leave other tooltip description elements as they are because they seem
    fine for the most part.

--- packages/shared-types/appointment.ts
+++ packages/shared-types/appointment.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const Appointment = z.object({
   id: z.string().min(1),
   title: z.string().min(1).max(60),
-  description: z.string().max(2048).nullable().default(""),
+  description: z.string().nullable().default(""),
   startDate: z.date(),
   endDate: z.date().nullish(),
   deletedAt: z.date().nullish(),
